2418dfa1864e2ced812a0141ed87abf9d93dc458
carson radtke <nosrac925@gmail.com>
2021-06-30 13:12:40 -0500

    just need to make http requests

--- osx/nasa_apod_background/nasa_apod_background/main.swift
+++ osx/nasa_apod_background/nasa_apod_background/main.swift
@@ -1,54 +1,91 @@
-//
-//  main.swift
-//  nasa_apod_background
-//
-//  Created by Carson Radtke on 6/28/21.
-//
-
 import Foundation
+import AppKit
 
 let endpoint: String = "https://api.nasa.gov"
-let apiKey: String = "<REDACTED>" // use 'DEMO_KEY' for testing
+let apiKey: String = "<REDACTED>" /* use 'DEMO_KEY' */
 
-func getImageData() -> APODObject? {
+let urlFormatString = "%@/planetary/apod?date=%@&api_key=%@&hd=true"
+let dateFormatString = "yyyy-MM-dd"
+
+func getURL() -> String
+{
+    let dateFormatter: DateFormatter = DateFormatter()
+    dateFormatter.dateFormat = dateFormatString
     
-    return nil
+    let dateString: String = dateFormatter.string(from: Date())
+    let urlString: String = String(format: urlFormatString, endpoint, dateString, apiKey)
     
+    return urlString
 }
 
-func getImage(apod: APODObject?) -> String? {
-   
-    if apod == nil {
-        return nil
+func getAPODResponse() -> APODObject
+{
+    let url = getURL()
+    
+    /* TODO */
+    
+    return APODObject()
+}
+
+func saveImage(url: String) -> NSURL
+{
+    let fileName: NSURL = NSURL(fileURLWithPath: String(format: "%@wallpaper.bmp", NSTemporaryDirectory()))
+    
+    /* TODO */
+    
+    return fileName
+}
+
+func setBackground(path: NSURL) -> Void
+{
+    do
+    {
+        let imgURL: URL = NSURL.fileURL(withPath: path.absoluteString!)
+        let workspace: NSWorkspace = NSWorkspace.shared
+        if let screen: NSScreen = NSScreen.main {
+            try workspace.setDesktopImageURL(imgURL, for: screen, options: [:])
+        }
+    }
+    catch
+    {
+        print(error)
+    }
+}
+
+func main() -> Void
+{
+    let apodObject: APODObject = getAPODResponse()
+
+    if (apodObject.media_type == "image")
+    {
+        let imagePath: NSURL = saveImage(url: apodObject.hdurl!)
+
+        setBackground(path: imagePath)
+    }
+}
+
+class APODObject
+{
+    init()
+    {
+        copyright = nil
+        date = nil
+        explanation = nil
+        hdurl = nil
+        media_type = nil
+        service_version = nil
+        title = nil
+        url = nil
     }
     
-    return nil
-    
+    let copyright: String?
+    let date: String?
+    let explanation: String?
+    let hdurl: String?
+    let media_type: String?
+    let service_version: String?
+    let title: String?
+    let url: String?
 }
 
-func setBackground(img: Any?) -> Void {
-    
-    if img == nil {
-        return
-    }
-    
-}
-
-func main() {
-    
-    setBackground(img: getImage(apod: getImageData()))
-    
-}
-
-main()
-
-struct APODObject {
-    var copyright: String? = nil
-    var date: String? = nil
-    var explanation: String? = nil
-    var hdurl: String? = nil
-    var media_type: String? = nil
-    var service_version: String? = nil
-    var title: String? = nil
-    var url: String? = nil
-}
+main();
